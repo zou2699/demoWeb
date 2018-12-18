435338a9fe33639afbe89b7eec0582cc3120da44
zou2699 <zou2699@gmail.com>
2018-12-18 10:07:16 +0800

    add third package

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
-	"io"
-	"log"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
+	//helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	//	io.WriteString(w, "Hello, world!\n")
+	//}
+	//
+	//http.HandleFunc("/", helloHandler)
+	//err := http.ListenAndServe(":8080", nil)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//log.Println("http listen 8080 successful")
 
-	http.HandleFunc("/", helloHandler)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Println("http listen 8080 successful")
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// This handler will match /user/john but will not match /user/ or /user
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	// However, this one will match /user/john/ and also /user/john/send
+	// If no other routers match /user/john, it will redirect to /user/john/
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	router.Run(":8080")
 }
